ecd4f89978770c28f45081b38e03c7dac51eee3a
morcohen1515 <morcohen1515@gmail.com>
2020-11-15 19:33:14 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000
+
+//handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.get('/about', (req, res) => res.render('about'));
+
+app.get('/menu', (req, res) => res.render('menu'));
+
+app.get('/order', (req, res) => res.render('order'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ views/data/jahnon.js
@@ -0,0 +1,17 @@
+export const jahnon_ar =[
+    {
+        "name":"גחנון אישי",
+        "price":"$",
+        "image":""
+    },
+    {
+        "name":"גחנון זוגי",
+        "price":"$",
+        "image":""
+    },
+    {
+        "name":"סיר גחנון",
+        "price":"$",
+        "image":""
+    }
+]
