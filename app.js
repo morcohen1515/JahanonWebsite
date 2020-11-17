ea7a75586f049ef4faa35e70f05e1252fdb87a69
morcohen1515 <morcohen1515@gmail.com>
2020-11-17 12:14:49 +0200

    image

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
 
 //handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {res.render('index');})
 
 app.get('/about', (req, res) => res.render('about'));
 
 app.get('/menu', (req, res) => res.render('menu'));
 
 app.get('/order', (req, res) => res.render('order'));
- 
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('conecct'))
